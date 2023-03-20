647dba14deaffa00201140e5340362f830cb49fd
genaroibc <bonavitaismael@gmail.com>
2023-03-20 11:43:57 -0300

    [DEV]: add constants and types

---
+++ app/constants/index.ts
@@ -0,0 +1,10 @@
+export const CHAINS_ID = {
+  1: "Mainnet",
+  3: "Ropsten",
+  4: "Rinkeby",
+  5: "Goerli",
+  42: "Kovan",
+  97: "BSC Testnet",
+  56: "Binance Smart Chain",
+  128: "Huobi ECO Chain",
+};

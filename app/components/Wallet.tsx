9ef84443e3e13ed98f5ad711f55402e745dd67fa
genaroibc <bonavitaismael@gmail.com>
2023-03-31 11:13:37 -0300

    [FEAT]: re-style and animate Wallet component

--- app/components/Wallet.tsx
+++ app/components/Wallet.tsx
@@ -1,26 +1,65 @@
 import { Account } from "../types";
 import { NetworkIcon } from "./NetworkIcon";
+import { useState } from "react";
+import { ArrowIcon, BalanceIcon } from "./shared/Icons";
 
 type Props = {
   account: Account;
 };
 
 export function Wallet({ account }: Props) {
-  return (
-    <article className="bg-blue-900 rounded text-center flex flex-col border-blue-900 border-2 border-solid shadow-2xl sm:min-w-[300px]">
-      <p className="text-white/80 bg-black/30 w-full p-4 font-medium">
-        {account.address.slice(0, 7) +
-          "..." +
-          account.address.slice(account.address.length - 5)}
-      </p>
-      <span className="text-3xl font-medium p-4">{account.balance} ETH</span>
+  const [isWalletOpen, setIsWalletOpen] = useState(false);
 
-      <span className="text-xl font-medium p-4 flex gap-2 items-center opacity-80">
-        <span className="w-4 inline-block">
-          <NetworkIcon />
-        </span>
-        <span className="text-sm">{account.networkName}</span>
-      </span>
-    </article>
+  const trimmedAddress =
+    account.address.slice(0, 7) +
+    "..." +
+    account.address.slice(account.address.length - 5);
+
+  return (
+    <section className="relative">
+      <button
+        onClick={() => setIsWalletOpen((currentStatus) => !currentStatus)}
+        className="font-medium flex items-center justify-center gap-2 cursor-pointer transition-all bg-slate-600/20 hover:bg-slate-600/30 active:bg-slate-600/50 px-4 rounded-xl text-base"
+        type="button"
+      >
+        <BalanceIcon />
+        <span>{trimmedAddress}</span>
+        <div
+          className="transition-transform"
+          style={{
+            transform: isWalletOpen ? "rotate(-180deg)" : "",
+          }}
+        >
+          <ArrowIcon />
+        </div>
+      </button>
+      <div
+        className="absolute top-full left-1/2 transition-transform duration-500"
+        style={{
+          animation: isWalletOpen
+            ? "fadeIn 0.5s linear both"
+            : "fadeOut 0.3s linear both",
+          transform: `translateX(-50%) ${
+            isWalletOpen ? "translateY(1rem)" : "translateY(0)"
+          }`,
+        }}
+      >
+        <article className="bg-blue-900 rounded text-center flex flex-col border-blue-900 border-2 border-solid shadow-2xl sm:min-w-[300px]">
+          <p className="text-white/80 bg-black/30 w-full p-4 font-medium">
+            {trimmedAddress}
+          </p>
+          <span className="text-3xl font-medium p-4">
+            {account.balance} ETH
+          </span>
+
+          <span className="text-xl font-medium p-4 flex gap-2 items-center opacity-80">
+            <span className="w-4 inline-block">
+              <NetworkIcon />
+            </span>
+            <span className="text-sm">{account.networkName}</span>
+          </span>
+        </article>
+      </div>
+    </section>
   );
 }

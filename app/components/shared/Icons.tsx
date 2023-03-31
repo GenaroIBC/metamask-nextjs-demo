1924c64bc3398246d2d4794ff71f9e76e9ea349e
genaroibc <bonavitaismael@gmail.com>
2023-03-31 11:12:49 -0300

    [DEV]: add icon components

---
+++ app/components/shared/Icons.tsx
@@ -0,0 +1,74 @@
+export function ArrowIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      aria-hidden="true"
+      width="24"
+      height="24"
+      className="transition-all rotate-0 hidden sm:block"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+        clip-rule="evenodd"
+      ></path>
+    </svg>
+  );
+}
+
+export function BalanceIcon() {
+  return (
+    <div className="rounded-[50px] inline-block m-0 overflow-hidden p-0 bg-[#fc6000] h-[20px] w-[20px]">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        x="0"
+        y="0"
+        height="20"
+        width="20"
+      >
+        <rect
+          x="0"
+          y="0"
+          rx="0"
+          ry="0"
+          height="20"
+          width="20"
+          transform="translate(1.4162945156275863 2.401538955119358) rotate(200.6 10 10)"
+          fill="#f72901"
+        ></rect>
+        <rect
+          x="0"
+          y="0"
+          rx="0"
+          ry="0"
+          height="20"
+          width="20"
+          transform="translate(-5.955624668531861 -7.752892004865809) rotate(281.7 10 10)"
+          fill="#018c84"
+        ></rect>
+        <rect
+          x="0"
+          y="0"
+          rx="0"
+          ry="0"
+          height="20"
+          width="20"
+          transform="translate(7.320022336738156 14.480228323895593) rotate(150.2 10 10)"
+          fill="#fc1975"
+        ></rect>
+        <rect
+          x="0"
+          y="0"
+          rx="0"
+          ry="0"
+          height="20"
+          width="20"
+          transform="translate(-26.102833657906192 4.656598945554891) rotate(310.4 10 10)"
+          fill="#c7145c"
+        ></rect>
+      </svg>
+    </div>
+  );
+}
